b9d837da60f0a247cd169341ab4fa6b005790328
Ming Duan <11203duan@gmail.com>
2019-04-02 19:37:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do
-      |student| self.initialize(student)
+      |student| self.new(student)
     end
   end
 
