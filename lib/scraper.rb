dabbffd38ad0408033f3901547d18fccf9c8295b
Ming Duan <11203duan@gmail.com>
2019-04-02 19:26:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,37 +24,20 @@ class Scraper
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
     doc.css("div.social-icon-container a").each do |x|
-    
+
       link = x.attr("href")
 
     @profile[:twitter] = link if link.include?("twitter")
     @profile[:github] = link if link.include?("github")
     @profile[:linkedin] = link if link.include?("linkedin")
-    @profile[:blog] = link if @profile.include?("link") == false
+    @profile[:blog] = link if @profile.has_value?(link) == false
 
+    end
 
-#     if x.css('a[href*="twitter"]') != nil
-#       @profile[:twitter] = x.css('a[href*="twitter"]').attr("href").value
-#     end
-#     if x.css('a[href*="linkedin"]') != nil
-#       @profile[:linkedin] = x.css('a[href*="linkedin"]').attr("href").value
-#     end
-#     if x.css('a[href*="github"]') != nil
-#       @profile[:github] = x.css('a[href*="github"]').attr("href").value
-#     end
-#
-#     if x.css('a img').attr("src").text.include?("rss")
-#       binding.pry
-#       @profile[:blog] = x.css('a').attr("href").value
-#
+    @profile[:profile_quote] = doc.css("div.profile-quote").text
+    @profile[:bio] = doc.css("div.description-holder p").text
 
-#
-# doc.css("div.social-icon-container").css('a img').attr("src").text
-#
+    @profile
+  end
 
-#
-#     @profile[:profile_quote] = doc.css("div.profile-quote").text
-#     @profile[:bio] = doc.css("div.description-holder p").text
-end
-end
 end
